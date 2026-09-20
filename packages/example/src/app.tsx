97952ec2df9a06053c58252a3a6da933ceba4275
Sergey Shishkin <sergey@shishkin.org>
2026-09-20 21:04:39 +0200

    fix(deps): maplibre 6

--- packages/example/src/app.tsx
+++ packages/example/src/app.tsx
@@ -1,6 +1,10 @@
+import { setWorkerUrl } from "maplibre-gl";
+import workerUrl from "maplibre-gl/dist/maplibre-gl-worker.mjs?worker&url";
 import { render } from "solid-js/web";
-import { Complete } from "./complete.jsx";
 import Click from "./click.jsx";
+import { Complete } from "./complete.jsx";
+
+setWorkerUrl(workerUrl);
 
 function App() {
   return (
